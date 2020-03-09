1d35b04f4d502c6cd04b7365551f10fad471f631
moniad <moni.dziedzic@gmail.com>
2020-03-09 21:07:27 +0100

    Added basic form submitting

---
+++ src/main/java/distributed_systems/lab2/homework/Input.java
@@ -0,0 +1,10 @@
+package distributed_systems.lab2.homework;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Input {
+    private String content;
+}
---
+++ src/main/java/distributed_systems/lab2/homework/RestApplication.java
@@ -0,0 +1,13 @@
+package distributed_systems.lab2.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApplication.class, args);
+	}
+
+}
